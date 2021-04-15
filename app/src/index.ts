87d07148fd1091e2207d6edc00459d639cadd822
dnovak252 <davidnovak252@gmail.com>
2021-04-15 13:50:56 +0200

    Add test routes

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,32 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.get('/', (req,res) => res.send('Hello world!'));
+
+app.get('/read', (req,res) =>{
+  var test = req.query;
+
+  console.log(test);
+})
+
+app.post('/create', (req, res)=>{
+  var test = req.body;
+
+  res.json(test);
+})
+
+app.delete('/delete/:id', (req,res) => {
+  res.send("DELETE request for " + req.params.id)
+})
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });

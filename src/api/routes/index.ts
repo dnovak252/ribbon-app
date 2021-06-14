13911fcf0a4f993389c265f4512f1999869838c7
dnovak252 <davidnovak252@gmail.com>
2021-06-14 14:49:31 +0200

    Add user routes

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import giftRouter from './GiftRouter';
+import userRouter from './UserRouter';
 
 
 const router = Router();
 
 router.use("/gifts", giftRouter);
+router.use("/users", userRouter);
 
 export default router;

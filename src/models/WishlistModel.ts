e35932ab6a930347a360de32645296f38d289ab5
dnovak252 <davidnovak252@gmail.com>
2021-06-14 14:46:09 +0200

    Add sequelize wishlist model

---
+++ src/models/WishlistModel.ts
@@ -0,0 +1,37 @@
+import { BuildOptions, DataTypes, IntegerDataType, Model, Sequelize } from "sequelize";
+
+export interface WishlistAttributes {
+  Id: string,
+  UserId: string,
+  GiftName: number,
+  GiftDescription: string,
+  GiftImage: string
+}
+
+export interface WishlistModel extends Model<WishlistAttributes>, WishlistAttributes{};
+export class Wishlist extends Model<WishlistModel, WishlistAttributes>{};
+
+export type WishlistStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): WishlistModel;
+};
+
+export function ChildFactory (sequelize: Sequelize): WishlistStatic{
+  return <WishlistStatic>sequelize.define("wishlist", {
+    Id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    UserId: {
+      type: DataTypes.STRING
+    },
+    GiftName: {
+      type: DataTypes.NUMBER
+    },
+    GiftDescription: {
+      type: DataTypes.STRING
+    },
+    GiftImage: {
+      type: DataTypes.STRING
+    }
+  }, {timestamps: false, freezeTableName: true});
+}
